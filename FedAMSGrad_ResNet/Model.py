ee1c958fa3d199f80f46c8e1a631c110f12a9fe2
sudongyuan <sudongyuan2019@email.szu.edu.cn>
2023-03-04 20:55:56 +0800

    Upload Code.

---
+++ FedAMSGrad_ResNet/Model.py
@@ -0,0 +1,260 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+import numpy as np
+import random
+import os
+
+myseed = 0
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+random.seed(myseed)
+os.environ['PYTHONHASHSEED'] = str(myseed)
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(myseed)
+    torch.cuda.manual_seed_all(myseed)
+
+
+# class Net(nn.Module):
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.model1 = nn.Sequential(
+#             nn.Conv2d(3, 32, 5, padding=2),
+#             nn.MaxPool2d(2),
+#             nn.Conv2d(32, 32, 5, padding=2),
+#             nn.MaxPool2d(2),
+#             nn.Conv2d(32, 64, 5, padding=2),
+#             nn.MaxPool2d(2),
+#             nn.Flatten(),
+#             nn.Linear(1024, 64),
+#             nn.Linear(64, 10)
+#         )
+#
+#     def forward(self, x):
+#         x = self.model1(x)
+#         return x
+
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         # 96->64 64->128
+#         self.conv1 = nn.Sequential(
+#             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(2, 2), padding=1),
+#             nn.Dropout(0.5)
+#         )
+#         self.conv2 = nn.Sequential(
+#             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 128, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 128, kernel_size=(3, 3), stride=(2, 2), padding=1),
+#             nn.Dropout(0.5)
+#         )
+#         self.conv3 = nn.Sequential(
+#             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 128, kernel_size=(1, 1), padding=0),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 10, kernel_size=(1, 1), padding=0),
+#         )
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.conv2(x)
+#         x = self.conv3(x)
+#         x = F.adaptive_avg_pool2d(x, (1, 1))
+#         x = torch.squeeze(x)
+#         return x
+
+
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Sequential(
+#             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(5, 5), padding=1),
+#             nn.ReLU(),
+#             nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+#             nn.Dropout(0.5)
+#         )
+#         self.conv2 = nn.Sequential(
+#             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(5, 5), padding=1),
+#             nn.ReLU(),
+#             nn.MaxPool2d(kernel_size=(3, 3), stride=2),
+#             nn.Dropout(0.5)
+#         )
+#         self.conv3 = nn.Sequential(
+#             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 128, kernel_size=(1, 1), padding=0),
+#             nn.ReLU(),
+#             nn.Conv2d(128, 10, kernel_size=(1, 1), padding=0),
+#         )
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.conv2(x)
+#         x = self.conv3(x)
+#         x = F.adaptive_avg_pool2d(x, (1, 1))
+#         x = torch.squeeze(x)
+#         return x
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Flatten(),
+            nn.Linear(in_features=1024, out_features=256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=256, out_features=64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(in_features=64, out_features=10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
+
+
+class ALL_CNN(nn.Module):
+    def __init__(self):
+        super(ALL_CNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(3, 3), padding=1),
+            nn.RReLU(),
+            nn.Conv2d(96, 96, kernel_size=(3, 3), padding=1),
+            nn.RReLU(),
+            nn.Conv2d(96, 96, kernel_size=(3, 3), stride=(2, 2), padding=1),
+            nn.Dropout(0.5)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3, 3), padding=1),
+            nn.RReLU(),
+            nn.Conv2d(192, 192, kernel_size=(3, 3), padding=1),
+            nn.RReLU(),
+            nn.Conv2d(192, 192, kernel_size=(3, 3), stride=(2, 2), padding=1),
+            nn.Dropout(0.5)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=(3, 3), padding=1),
+            nn.RReLU(),
+            nn.Conv2d(192, 192, kernel_size=(1, 1), padding=0),
+            nn.RReLU(),
+            nn.Conv2d(192, 10, kernel_size=(1, 1), padding=0),
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        x = torch.squeeze(x)
+        return x
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=True),
+            nn.BatchNorm2d(outchannel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(outchannel)
+        )
+        self.shortcut = nn.Sequential()
+        if stride != 1 or inchannel != outchannel:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(inchannel, outchannel, kernel_size=1, stride=stride, bias=True),
+                nn.BatchNorm2d(outchannel)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, ResidualBlock, num_classes=10):
+        super(ResNet, self).__init__()
+        self.inchannel = 64
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(ResidualBlock, 64,  2, stride=1)
+        self.layer2 = self.make_layer(ResidualBlock, 128, 2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 256, 2, stride=2)
+        self.layer4 = self.make_layer(ResidualBlock, 512, 2, stride=2)
+        self.fc = nn.Linear(512, num_classes)
+
+    def make_layer(self, block, channels, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)   #strides=[1,1]
+        layers = []
+        for stride in strides:
+            layers.append(block(self.inchannel, channels, stride))
+            self.inchannel = channels
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+def ResNet18(num_classes=10):
+
+    return ResNet(ResidualBlock, num_classes=num_classes)
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x):
+        return self.fn(x) + x
+
+def ConvMixer(dim, depth, kernel_size=5, patch_size=2, n_classes=10):
+    return nn.Sequential(
+        nn.Conv2d(3, dim, kernel_size=patch_size, stride=patch_size),
+        nn.GELU(),
+        nn.BatchNorm2d(dim),
+        *[nn.Sequential(
+            Residual(nn.Sequential(
+                nn.Conv2d(dim, dim, kernel_size, groups=dim, padding="same"),
+                nn.GELU(),
+                nn.BatchNorm2d(dim)
+            )),
+            nn.Conv2d(dim, dim, kernel_size=1),
+            nn.GELU(),
+            nn.BatchNorm2d(dim)
+        ) for i in range(depth)],
+        nn.AdaptiveAvgPool2d((1, 1)),
+        nn.Flatten(),
+        nn.Linear(dim, n_classes)
+    )
---
+++ FedCAMS_ResNet/topk256/Clients.py
@@ -0,0 +1,159 @@
+from torchvision import datasets, transforms, models
+import torch
+from torch import nn
+import Model as m
+import numpy as np
+import time
+import heapq
+import math
+import random
+import os
+import scipy.stats as st
+from scipy.optimize import curve_fit
+
+myseed = 0
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+os.environ['PYTHONHASHSEED'] = str(myseed)
+random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(myseed)
+    torch.cuda.manual_seed_all(myseed)
+
+batch_size = 20
+epoch_num = 3
+
+transform_train = transforms.Compose([
+    # transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+    # transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    # transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# device = torch.device("cpu")
+
+
+class Client():
+    def __init__(self, client_num):
+        self.client_num = client_num
+        self.model = torch.load("../../init_resnet.pkl")  # m.ResNet18()  # m.ResNet(m.ResidualBlock)
+        self.last_model = torch.load("../../init_resnet.pkl")  # m.ResNet18()
+        print(self.model)
+        train_data = datasets.CIFAR10('../../../CIFAR-10', train=True, download=True,
+                                      transform=transform_train)
+        self.lr = 0.01
+        self.data_len = []
+        self.communication_time = [0] * self.client_num
+        self.train_loader = []
+
+        total_trainable_params = 0
+        for p in self.model.parameters():
+            total_trainable_params += p.numel()
+        print("total parameter num is ", total_trainable_params)
+        self.proportion = 1
+        self.d = total_trainable_params
+        self.traffic = self.d * self.proportion * (32 + int(math.log(self.d, 2)) + 1) / 8 / 1024 / 1024
+
+        self.accu_gra = torch.zeros((client_num, total_trainable_params))
+
+        print(range(len(train_data)))
+        data_len = int(50000 / self.client_num)
+        print("data len is ", data_len)
+
+        # index_list = list(range(len(train_data)))
+        # random.shuffle(index_list)
+
+        for i in range(self.client_num):
+            self.data_len.append(data_len)
+            start_index = int(len(train_data) / self.client_num) * i
+            print("the start index is ", start_index)
+            data = []
+            for j in range(data_len):
+                idx = (start_index + j) % len(train_data)
+                # data.append(train_data[index_list[idx]])
+                data.append(train_data[idx])
+            train_loader = torch.utils.data.DataLoader(
+                data,
+                batch_size=batch_size, shuffle=True
+            )
+            self.train_loader.append(train_loader)
+            print(i)
+
+    def get_dataLen(self, idx):
+        return self.data_len[idx]
+
+    def change_lr(self, learning_rate):
+        self.lr = [learning_rate] * self.client_num
+        print("now lr is ", self.lr)
+
+    def get_model(self):
+        return self.model
+
+    def get_model_gradient(self, idx):
+        parameters = torch.tensor([])
+        num_batches_tracked = 0
+        for p in self.model.parameters():
+            parameters = torch.cat((parameters, p.view(-1).detach()))
+        last_parameters = torch.tensor([])
+        for p in self.last_model.parameters():
+            last_parameters = torch.cat((last_parameters, p.view(-1).detach()))
+        self.accu_gra[idx] = parameters - last_parameters - self.accu_gra[idx]
+
+        proportion = 1 / 256
+        value, indices = torch.topk(self.accu_gra[idx].abs(), int(self.accu_gra[idx].size()[0] * proportion))
+        print("top {}% is {}".format(proportion * 100, torch.min(value)))
+
+        new_gra = torch.zeros(self.accu_gra[idx].shape)
+        new_gra[indices] = self.accu_gra[idx][indices]
+        self.accu_gra[idx][indices] = 0
+        return new_gra, num_batches_tracked
+
+    def set_model(self, model):
+        self.model.load_state_dict(model.state_dict())
+        self.last_model.load_state_dict(model.state_dict())
+        # torch.save(model, "net_params.pkl")
+        # self.model = torch.load("net_params.pkl")
+        # self.last_model = torch.load("net_params.pkl")
+
+    def adjust_learning_rate(self):
+        self.lr *= 0.5
+        # for param_group in optimizer.param_groups:
+        #     param_group['lr'] = param_group['lr'] * decay_rate
+        #     self.lr = param_group['lr']
+
+    def train(self, idx, communication_time):
+        self.communication_time[idx] += 1
+        print("client no is ", idx, " train num is ", self.communication_time[idx])
+        self.model = self.model.to(device)
+        loss_fn = torch.nn.CrossEntropyLoss().to(device)
+
+        lr = self.lr
+        print("learning rate is ", lr)
+        # optimizer = torch.optim.SGD(params=self.model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-6,
+        #                             nesterov=True)
+
+        optimizer = torch.optim.SGD(params=self.model.parameters(), lr=lr)
+        for epo in range(epoch_num):
+            self.model.train()
+            for i, (data, label) in enumerate(self.train_loader[idx]):
+                data = data.to(device)
+                label = label.to(device)
+
+                optimizer.zero_grad()
+
+                pred = self.model(data)
+                loss = loss_fn(pred, label)
+                loss.backward()
+                optimizer.step()
+
+                if i % 100 == 0:
+                    print("Train Epoch: {}, iteration: {}, Loss: {}".format
+                          (epo, i, loss.item()))
+
+        self.model = self.model.to('cpu')
