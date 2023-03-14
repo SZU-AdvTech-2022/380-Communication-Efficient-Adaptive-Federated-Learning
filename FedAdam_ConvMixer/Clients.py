bbe1b992da9ec0a854237059d04dc5b3a38f3705
szu-advtech <none>
2023-03-14 17:56:09 +0800

    Initial commit

---
+++ FedAdam_ConvMixer/Clients.py
@@ -0,0 +1,208 @@
+from torchvision import datasets, transforms, models
+import torch
+from torch import nn
+import Model as m
+import numpy as np
+import time
+import heapq
+import math
+import random
+import os
+import scipy.stats as st
+from scipy.optimize import curve_fit
+
+myseed = 0
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+os.environ['PYTHONHASHSEED'] = str(myseed)
+random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(myseed)
+    torch.cuda.manual_seed_all(myseed)
+
+batch_size = 20
+epoch_num = 3
+
+transform_train = transforms.Compose([
+    # transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+    # transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+    transforms.ToTensor(),
+    # transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# device = torch.device("cpu")
+
+
+class Client():
+    def __init__(self, client_num):
+        self.client_num = client_num
+        self.model = torch.load(
+            "../init_conv_model.pkl")  # m.ConvMixer(256, 8, patch_size=2, kernel_size=5, n_classes=10)
+        self.last_model = torch.load(
+            "../init_conv_model.pkl")  # m.ConvMixer(256, 8, patch_size=2, kernel_size=5, n_classes=10)
+        print(self.model)
+        train_data = datasets.CIFAR10('../../CIFAR-10', train=True, download=True,
+                                      transform=transform_train)
+        self.lr = 0.01
+        self.data_len = []
+        self.communication_time = [0] * self.client_num
+        self.train_loader = []
+
+        total_trainable_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print("total parameter num is ", total_trainable_params)
+        self.proportion = 1
+        self.d = total_trainable_params
+        self.traffic = self.d * self.proportion * (32 + int(math.log(self.d, 2)) + 1) / 8 / 1024 / 1024
+
+        self.accu_gra = torch.zeros((client_num, total_trainable_params))
+
+        print(range(len(train_data)))
+        data_len = int(50000 / self.client_num)
+        print("data len is ", data_len)
+
+        # index_list = list(range(len(train_data)))
+        # random.shuffle(index_list)
+
+        # for i in range(self.client_num):
+        #     self.data_len.append(data_len)
+        #     start_index = int(len(train_data) / self.client_num) * i
+        #     print("the start index is ", start_index)
+        #     data = []
+        #     for j in range(data_len):
+        #         idx = (start_index + j) % len(train_data)
+        #         # data.append(train_data[index_list[idx]])
+        #         data.append(train_data[idx])
+        #     train_loader = torch.utils.data.DataLoader(
+        #         data,
+        #         batch_size=batch_size, shuffle=True
+        #     )
+        #     self.train_loader.append(train_loader)
+        #     print(i)
+        self.label_index = [[] for i in range(10)]
+        for i, (d, l) in enumerate(train_data):
+            self.label_index[l].append(i)
+        start_index = [0] * 10
+        choose_label = list(range(10))
+        for i in range(self.client_num):
+            print(i)
+            for j in range(10):
+                if (start_index[j] > (5000 - int(data_len / 5))) and (j in choose_label):
+                    choose_label.remove(j)
+            all_samples = []
+            if i % 2 == 0:
+                label = random.sample(choose_label, 5)
+            else:
+                label = [val for val in choose_label if val not in label]
+            for j in label:
+                all_samples.extend(random.sample
+                                   (self.label_index[j][start_index[j]:start_index[j] + int(data_len / 5)],
+                                    int(data_len / 5)))
+                start_index[j] += int(data_len / 5)
+            print("the choose label are ", label)
+            print("data_len is ", len(all_samples))
+            self.data_len.append(len(all_samples))
+            data = []
+            for j in all_samples:
+                data.append(train_data[j])
+            train_loader = torch.utils.data.DataLoader(
+                data,
+                batch_size=batch_size, shuffle=True,
+                num_workers=0
+            )
+            self.train_loader.append(train_loader)
+
+    def get_dataLen(self, idx):
+        return self.data_len[idx]
+
+    def change_lr(self, learning_rate):
+        self.lr = [learning_rate] * self.client_num
+        print("now lr is ", self.lr)
+
+    def get_model(self):
+        return self.model
+
+    def get_model_gradient(self, idx):
+        parameters = torch.tensor([])
+        num_batches_tracked = 0
+        for p in self.model.parameters():
+            parameters = torch.cat((parameters, p.view(-1).detach()))
+        last_parameters = torch.tensor([])
+        for p in self.last_model.parameters():
+            last_parameters = torch.cat((last_parameters, p.view(-1).detach()))
+        # self.accu_gra[idx] = last_parameters - parameters + self.accu_gra[idx]
+        #
+        # i = 0
+        # for k, v in self.model.named_parameters():
+        #     if v.requires_grad == False:
+        #         print("the unGrad module name is {}".format(k))
+        #         continue
+        #     length = v.numel()
+        #     print("the grad module name is {}, the min value is {}, the max value is {}".format(k, torch.min(
+        #         self.accu_gra[idx][i:i + length]), torch.max(self.accu_gra[idx][i:i + length])))
+        #     i += length
+        #
+        # value, indices = torch.topk(self.accu_gra[idx].abs(), int(self.accu_gra[idx].size()[0] * self.proportion))
+        # print("top {}% is {}".format(self.proportion * 100, torch.min(value)))
+        #
+        # new_gra = torch.zeros(self.accu_gra[idx].shape)
+        # new_gra[indices] = self.accu_gra[idx][indices]
+        # self.accu_gra[idx][indices] = 0
+        # return new_gra
+
+        # file_handle = open('1.txt', mode='a')
+        # result = torch.sort(torch.abs(last_parameters - parameters), descending=True).cpu().numpy().tolist()
+        # strNums = [str(x_i) for x_i in result]
+        # str1 = ",".join(strNums)
+        # file_handle.write(str1+'\n')
+
+        return parameters - last_parameters, num_batches_tracked
+
+    def set_model(self, model):
+        self.model.load_state_dict(model.state_dict())
+        self.last_model.load_state_dict(model.state_dict())
+        # torch.save(model, "net_params.pkl")
+        # self.model = torch.load("net_params.pkl")
+        # self.last_model = torch.load("net_params.pkl")
+
+    def adjust_learning_rate(self):
+        self.lr *= 0.5
+        # for param_group in optimizer.param_groups:
+        #     param_group['lr'] = param_group['lr'] * decay_rate
+        #     self.lr = param_group['lr']
+
+    def train(self, idx, communication_time):
+        self.communication_time[idx] += 1
+        print("client no is ", idx, " train num is ", self.communication_time[idx])
+        self.model = self.model.to(device)
+        loss_fn = torch.nn.CrossEntropyLoss().to(device)
+
+        lr = self.lr
+        print("learning rate is ", lr)
+        # optimizer = torch.optim.SGD(params=self.model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-6,
+        #                             nesterov=True)
+
+        optimizer = torch.optim.SGD(params=self.model.parameters(), lr=lr)
+        for epo in range(epoch_num):
+            self.model.train()
+            for i, (data, label) in enumerate(self.train_loader[idx]):
+                data = data.to(device)
+                label = label.to(device)
+
+                optimizer.zero_grad()
+
+                pred = self.model(data)
+                loss = loss_fn(pred, label)
+                loss.backward()
+                optimizer.step()
+
+                if i % 100 == 0:
+                    print("Train Epoch: {}, iteration: {}, Loss: {}".format
+                          (epo, i, loss.item()))
+
+        self.model = self.model.to('cpu')
---
+++ FedCAMS_ConvMixer/sign/Server.py
@@ -0,0 +1,154 @@
+import torch
+from torch import nn
+from torchvision import datasets, transforms, models
+import Clients
+import Model as m
+import random
+import numpy as np
+import ssl
+import os
+import math
+import scipy.stats as st
+
+ssl._create_default_https_context = ssl._create_unverified_context
+myseed = 0
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+random.seed(myseed)
+os.environ['PYTHONHASHSEED'] = str(myseed)
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(myseed)
+    torch.cuda.manual_seed_all(myseed)
+
+# device = torch.device("cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+client_num = 100
+select_num = 10
+batch_size = 20
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    # R,G,B每层的归一化用到的均值和方差
+])
+if __name__ == '__main__':
+    tmp_model = torch.load("../../init_conv_model.pkl")  # m.ResNet18()
+    model = torch.load("../../init_conv_model.pkl")  # m.ResNet18()  # torch.load("net_params.pkl")
+    d = 0
+    for p in model.parameters():
+        # if p.find("num_batches_tracked") != -1:
+        #     continue
+        d += p.numel()
+    print(f'{d:,} training parameters.')
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../../../CIFAR-10', train=False, download=True,
+                         transform=transform_test),
+        batch_size=batch_size, shuffle=False
+    )
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('../../../CIFAR-10', train=True, download=True,
+                         transform=transform_test),
+        batch_size=batch_size, shuffle=True
+    )
+
+    # 确定客户端的数量，从而进行数据的划分
+    clients = Clients.Client(client_num)
+
+    communication_time = 0  # 记录当前第几轮通信
+
+    m = torch.zeros(d)
+    v = torch.zeros(d)
+    vhat = torch.zeros(d)
+    epsilon = torch.ones(d) * 0.001
+
+    beta1 = 0.9
+    beta2 = 0.99
+    eta = 1
+
+    # 一个迭代回合代表一次全局通信
+    while True:
+        if communication_time % 5 == 0:
+            acc = 0
+            train_loss = 0.
+            correct = 0.
+            total = 0.
+            loss_fn = torch.nn.CrossEntropyLoss()
+            model = model.to(device)
+
+            with torch.no_grad():
+                model.train()
+                for (data, target) in test_loader:
+                    data = data.to(device)
+                    target = target.to(device)
+
+                    output = model(data)
+                    pred = output.argmax(dim=1)
+                    correct += pred.eq(target.view_as(pred)).sum().item()
+                    total += target.size(0)
+                acc = correct / total * 100.
+                print("Communication Time is ", communication_time)
+                print("Accuracy: {}".format(acc))
+
+                # for (data, target) in train_loader:
+                #     data = data.to(device)
+                #     target = target.to(device)
+                #     output = model(data)
+                #     train_loss += loss_fn(output, target)
+                # train_loss /= len(train_loader)
+                # print("Train loss: {}".format(train_loss))
+            model = model.to('cpu')
+            if communication_time >= 200:
+                print("Communication Time is ", communication_time)
+                break
+        select_clients = random.sample(list(range(client_num)), select_num)
+
+        model_gra = torch.zeros(d)  # 全局梯度
+
+        for j in select_clients:
+            print("the select client num is {}".format(j))
+            clients.set_model(model)
+
+            clients.train(j, communication_time)
+
+            local_model_gra, num_batches_tracked = clients.get_model_gradient(j)  # 客户端的时间,同时包括上传\重传请求
+
+            model_gra += 1 / select_num * local_model_gra
+
+        # m = beta1 * m + (1 - beta1) * model_gra
+        # v = beta2 * v + (1 - beta2) * (model_gra ** 2)
+        # vhat = torch.max(vhat, torch.max(v, epsilon))
+        #
+        # model_gra = eta * m / torch.sqrt(vhat)
+        # model_gra = eta * m
+
+        idx = 0
+        # new_model = model.state_dict()
+        for p in model.parameters():
+            # if p.find("num_batches_tracked") != -1:
+            #     continue
+            size = p.numel()
+            # if p.find("running"):
+            #     new_model[p].data = new_model[p].data + model_gra[idx:idx + size].reshape(new_model[p].shape).data
+            # else:
+            m[idx:idx + size] = beta1 * m[idx:idx + size] + (1 - beta1) * model_gra[idx:idx + size]
+            v[idx:idx + size] = beta2 * v[idx:idx + size] + (1 - beta2) * (model_gra[idx:idx + size] ** 2)
+            vhat[idx:idx + size] = torch.max(vhat[idx:idx + size],
+                                             torch.max(v[idx:idx + size], epsilon[idx:idx + size]))
+
+            model_gra[idx:idx + size] = eta * m[idx:idx + size] / torch.sqrt(vhat[idx:idx + size])
+
+            # new_model[p].data = new_model[p].data + model_gra[idx:idx + size].reshape(new_model[p].shape).data
+            p.data = p.data + model_gra[idx:idx + size].reshape(p.shape).data
+            idx += size
+        # model.load_state_dict(new_model)
+
+
+        communication_time += 1
+        print("Communication Time is ", communication_time)
+
+
